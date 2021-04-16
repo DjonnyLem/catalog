8c6ad03e60ab317f93957f375ec4f4f19ba27b76
DjonnyLem <djonnylem@gmail.com>
2021-04-16 22:43:10 +0400

    16/04/21 22:43

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
-from flask_sqlalchemy import  SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
@@ -9,30 +9,36 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 class Catalog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    defect = db.Column(db.Text, db.ForeignKey('defect.defect_name'))#,, nullable=False)
-    operation = db.Column(db.Text, db.ForeignKey('operation.operation_name'), nullable=False)
-    aricle = db.Column(db.Text)#, nullable=False)
-    name =  db.Column(db.Text)#,, nullable=False)
-    sort = db.Column(db.Integer)#,, nullable=False)
-    #picture =
-    note = db.Column(db.String(300))#,, nullable=False)
+    defect = db.Column(db.Text, db.ForeignKey(
+        'defect.defect_name'))  # ,, nullable=False)
+    operation = db.Column(db.Text, db.ForeignKey(
+        'operation.operation_name'), nullable=False)
+    aricle = db.Column(db.Text)  # , nullable=False)
+    name = db.Column(db.Text)  # ,, nullable=False)
+    sort = db.Column(db.Integer)  # ,, nullable=False)
+    # picture =
+    note = db.Column(db.String(300))  # ,, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
-   
     def __repr__(self):
-        return "<User %r, %r, %r, %r, %r, %r, %r>" %(self.defect, self.operation,
-                self.aricle, self.name, self.sort,
-                self.note,  self.date)
+        return "<User %r, %r, %r, %r, %r, %r, %r>" % (self.defect, self.operation,
+                                                      self.aricle, self.name, self.sort,
+                                                      self.note,  self.date)
+
 
 class Defect(db.Model):
     defect_name = db.Column(db.String(50), nullable=False, primary_key=True)
+
     def __repr__(self):
-        return   '<defect_name %r>' % self.defect_name
+        return '<defect_name %r>' % self.defect_name
+
 
 class Operation(db.Model):
     operation_name = db.Column(db.String(50), nullable=False, primary_key=True)
+
     def __repr__(self):
         return '<operation_name %r>' % self.operation_name
 
@@ -40,18 +46,19 @@ class Operation(db.Model):
 #        return "<User(%r, %r)>" % (
 #                self.name, self.fullname
 
-#def __init__(self, site_code, site_type, temperature, name):
+# def __init__(self, site_code, site_type, temperature, name):
 #        self.name = name
 #        self.site_code = site_code
 #        self.site_type = site_type
 #        self.temperature = temperature
+
+
 @app.route('/')
 @app.route('/home')
 def index():
-    #return  """ <h2 style='color: red;'>Hi, Djonny!</h2> """
+    # return  """ <h2 style='color: red;'>Hi, Djonny!</h2> """
 
     return render_template("base1.html")
-
 
 
 @app.route('/add', methods=['POST', 'GET'])
@@ -59,20 +66,21 @@ def add():
     e = request.form
     defect = Defect.query.all()
     operation = Operation.query.all()
-    catalog = Catalog.query.all()   
+    catalog = Catalog.query.all()
     for i in e:
         print(i)
     if request.method == "POST":
-        
+
         if request.form["indetify"] == "form1":
-              
+
             defect_name = (request.form['defect_name1']).upper()
             operation_name = (request.form['operation_name1']).upper()
             defect_type = (request.form['defect_type'])
             article_number = (request.form['article_number'])
             article_name = (request.form['article_name']).upper()
             note = (request.form['note']).upper()
-            print(defect_name, operation_name, defect_type, article_number, article_name) 
+            print(defect_name, operation_name, defect_type,
+                  article_number, article_name)
 
             q = Catalog(defect=defect_name,
                         operation=operation_name,
@@ -80,14 +88,14 @@ def add():
                         aricle=article_number,
                         name=article_name,
                         note=note)
-            
+
 #            defect = Catalog(defect=defect_name)
 #            operation = Catalog(operation=operation_name)
 #            sort = Catalog(sort=defect_type)
 #            aricle = Catalog(aricle=article_number)
 #            name = Catalog(name=article_name)
 #            note = Catalog(note1=note)
-             
+
             try:
                 db.session.add(q)
  #               db.session.add(operation)
@@ -99,11 +107,10 @@ def add():
             except:
                 return "При добавлении статьи произошла ошибка"
 
-        
         if request.form["indetify"] == "form2":
             defect_name = (request.form['defect_name']).upper()
             defect = Defect(defect_name=defect_name)
-        
+
             try:
                 db.session.add(defect)
                 db.session.commit()
@@ -115,7 +122,7 @@ def add():
         if request.form["indetify"] == "form3":
             operation_name = (request.form['operation_name']).upper()
             operation = Operation(operation_name=operation_name)
-        
+
             try:
                 db.session.add(operation)
                 db.session.commit()
@@ -125,25 +132,24 @@ def add():
 
     else:
         return render_template("add.html", operation=operation, defect=defect)
-     
-    
-@app.route('/show', methods=['POST', 'GET'])
 
+
+@app.route('/show', methods=['POST', 'GET'])
 def show():
     req = request.form.get("flexRadioDefault")
     defects = Defect.query.all()
     #d = Catalog.query.order_by(Catalog.operation).all()
     c = Catalog.query.all()
-    f=Catalog.query.order_by(Catalog.name).all()
+    f = Catalog.query.order_by(Catalog.name).all()
     s = Catalog.query.filter_by(defect='ПЕРЕКОС').all()
-    #print(defects)
-    #print(type(d))
-    #print(c)
-    #print('+'*70)
-    #print(f)
-    #print(req)
-    #for i in req:
-        #print (i)
+    # print(defects)
+    # print(type(d))
+    # print(c)
+    # print('+'*70)
+    # print(f)
+    # print(req)
+    # for i in req:
+    #print (i)
     if req == "a":
         d = Catalog.query.order_by(Catalog.defect).all()
     elif req == "b":
@@ -152,31 +158,38 @@ def show():
         d = Catalog.query.order_by(Catalog.id).all()
     return render_template("show.html", defects=defects, d=d, s=s)
 
-@app.route('/show_defect', methods=['POST', 'GET'])
+
+@app.route('/show_defect')
 def show_defect():
     sd = request.form.get('1234')
     se = request.form.get('12')
     defects = Defect.query.order_by(Defect.defect_name).all()
     d = Catalog.query.order_by(Catalog.defect).all()
-    print (sd)
-    print (se)
+    print(sd)
+    print(se)
     return render_template("show_defect.html", defects=defects, d=d)
 
 
+@app.route('/show_defect/<int:id>')
+def show_defect_detail(id):
+    defect_detail = Catalog.query.get(id)
 
-@app.route('/show_operation', methods=['POST', 'GET'])        
+    return render_template("defect_detail.html", defect_detail=defect_detail)
+
+
+@app.route('/show_operation')
 def show_operation():
     operations = Operation.query.order_by(Operation.operation_name).all()
-    
+
     return render_template("show_operation.html", operations=operations)
-      
+
 
 @app.route('/1')
-
 def one():
     pr = ["perfect", "beautiful", "nice"]
     defect = Defect.query.all()
-    return render_template("1.html", pr=pr,defect=defect)
+    return render_template("1.html", pr=pr, defect=defect)
+
 
 @app.route('/defect')
 def defect():
@@ -184,51 +197,49 @@ def defect():
     return render_template("defect.html")
 
 
-
-
 #    def __repr__(self):
 #        return '<Article %r>' % self.id
-    
-#class Article1(db.Model):
+
+# class Article1(db.Model):
 #    id = db.Column(db.Integer, primary_key=True)
 #   defect = db.Column(db.Text, nullable=False)
 #    operation = db.Column(db.Text, nullable=False)
 #    aricle = db.Column(db.Text, nullable=False)
 #    name =  db.Column(db.Text, nullable=False)
     #sort = db.Column(db.Integer, nullable=True)
-    #picture =
+    # picture =
 #    note1 = db.Column(db.String(300), nullable=False)
 #    note2 = db.Column(db.String(300), nullable=False)
 #    date = db.Column(db.DateTime, default=datetime.utcnow)
-    
+
 #    def __repr__(self):
 #        return '<Article1 %r>' % self.id
 
-#@app.route('/')
-#@app.route('/home')
-#def index():
+# @app.route('/')
+# @app.route('/home')
+# def index():
 #    return render_template("index.html")
 
 
-#@app.route('/about')
-#def about():
+# @app.route('/about')
+# def about():
 #    return render_template("about.html")
 
 
-#@app.route('/posts')
-#def posts():
+# @app.route('/posts')
+# def posts():
 #    articles = Article.query.order_by(Article.date.desc()).all()
 #    return render_template("posts.html", articles=articles)
 
 
-#@app.route('/posts/<int:id>')
-#def post_detail(id):
+# @app.route('/posts/<int:id>')
+# def post_detail(id):
 #    article = Article.query.get(id)
 #    return render_template("post-detail.html", article=article)
 
 
-#@app.route('/posts/<int:id>/del')
-#def post_delete(id):
+# @app.route('/posts/<int:id>/del')
+# def post_delete(id):
 #    article = Article.query.get_or_404(id)
 
 #    try:
@@ -239,8 +250,8 @@ def defect():
 #        return "При удалении статьи произошла ошибка"
 
 
-#@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
-#def post_update(id):
+# @app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+# def post_update(id):
 #    article = Article.query.get(id)
 #    if request.method == "POST":
 #        article.title = request.form['title']
@@ -256,10 +267,9 @@ def defect():
 #        return render_template("post_update.html", article=article)
 
 
+# @app.route('/create-article', methods=['POST', 'GET'])
 
-#@app.route('/create-article', methods=['POST', 'GET'])
-
-#def create_article():
+# def create_article():
 #    if request.method == "POST":
 #        title = request.form['title']
 #        intro = request.form['intro']
@@ -275,7 +285,6 @@ def defect():
 #            return "При добавлении статьи произошла ошибка"
 #    else:
 #        return render_template("create-article.html")
-        
 
 
 if __name__ == "__main__":
