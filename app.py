8c07a6944fc5d82b4b6c9a1a7f93584f9d274519
DjonnyLem <djonnylem@gmail.com>
2021-04-26 16:16:29 +0400

    26/04/2021 16:16

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from menu import menu
-
+#import upload
 
 
 app = Flask(__name__)
@@ -199,6 +199,73 @@ def defect():
     return render_template("defect.html")
 
 
+#app.config["IMAGE_UPLOADS"] = "/home/tech-3/Рабочий стол/test/app/static/img/uploads"
+#app.config["IMAGE_UPLOADS"] = "/home/lem/PROJECTS/test/app/static/img/uploads"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+app.config["MAX_IMAGE_FILESIZE"] = 4 * 1024 * 1024
+
+
+
+def allowed_image(filename):
+
+        if not "." in filename:
+                return False
+
+        ext = filename.rsplit(".", 1)[1]
+
+        if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+                return True
+        else:
+                return False
+
+def allowed_image_filesize(filesize):
+        
+        if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+                return True
+        else:
+                return False
+                
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+
+        if request.method == "POST":
+
+                if request.files:
+
+                        if "filesize" in request.cookies:
+
+                                if not allowed_image_filesize(request.cookies["filesize"]):
+                                        print("Filesize exceeded maximum limit")
+                                        return redirect(request.url)
+
+                        image = request.files["image"]
+
+                        if image.filename == "":
+                                print("No filename")
+                                return redirect(request.url)
+
+                        if allowed_image(image.filename):
+                                filename = secure_filename(image.filename)
+
+                                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+                                print("Image saved")
+
+                                return redirect(request.url)
+
+                        else:
+                                print("That file extension is not allowed")
+                                return redirect(request.url)
+
+        return render_template("upload.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+    
 #    def __repr__(self):
 #        return '<Article %r>' % self.id
 
@@ -288,6 +355,3 @@ def defect():
 #    else:
 #        return render_template("create-article.html")
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
