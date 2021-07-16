147824508cacc2833ada233c321cce0ea6b6e5c0
DjonnyLem <djonnylem@gmail.com>
2021-07-16 19:21:46 +0400

    16.07.21 19:21

--- static/js/base.js
+++ static/js/base.js
@@ -1,14 +1,31 @@
  function shineLinks(id) {
-      
-          var el = document.getElementById(id).getElementsByTagName('a');
-          var url = document.location.href;
-console.log(el);
+  console.log("Айгуль, я тебя люблю!")
+  console.log(urlName().nm)
+          let el = document.getElementById(id).getElementsByTagName('a');
+          // var url = document.location.href;
+// console.log(el);
           for (var i = 0; i < el.length; i++) {
-            console.log(url);
+            console.log(urlName().nm);
 			console.log(i,'=',el[i].href);
-            if (url == el[i].href) {
+
+            if (urlName().nm == el[i].href) {
 				el[i].className += " active";
             };
           };
        
       };
+
+
+function urlName (){
+  let url  = document.location.href; // получаем href страницы
+  let nm = url.match(/(?<=\/\/.+\/)\w+/); //Получаем из htef блок наименования страницы
+  nm = nm[0]
+  let num = url.match(/(?<=\/\/.+\/\w+\/)\d{1,}/);
+  if (num == null){
+    num = '0';
+    }
+  else {
+    num = num[0]
+    };
+   return {nm, num}
+};
